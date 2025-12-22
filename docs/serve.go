d8fd3f40bb10d96088db4b9b79b16c874d5c21e5
Hao-yiwen <1552975589@qq.com>
2025-12-23 00:39:18 +0800

    Refactor HTTP server setup in serve.go to use a variable for the file server handler

--- docs/serve.go
+++ docs/serve.go
@@ -6,9 +6,10 @@ import (
 )
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("./")))
+	// http.Handle("/", http.FileServer(http.Dir("./")))
 	fmt.Println("📚 文档服务器启动成功！")
 	fmt.Println("🌐 访问地址: http://localhost:8080/doc/")
 	fmt.Println("按 Ctrl+C 停止服务器")
-	http.ListenAndServe(":8080", nil)
+	hdl := http.FileServer(http.Dir("./"))
+	http.ListenAndServe(":8080", hdl)
 }
